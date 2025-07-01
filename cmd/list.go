4ce5835882c288b5594f5040aa9e124c2dbf1484
Landan Quartemont <landan@grease.solutions>
2025-07-01 17:00:03 -0600

    updated the list command

--- cmd/list.go
+++ cmd/list.go
@@ -1,27 +1,45 @@
 /*
 Copyright © 2025 NAME HERE <EMAIL ADDRESS>
-
 */
 package cmd
 
 import (
 	"fmt"
+	"log"
+	"os"
 
+	"github.com/landanqrew/simple-jot/internal/notes"
+	"github.com/landanqrew/simple-jot/internal/storage"
+	"github.com/olekukonko/tablewriter"
 	"github.com/spf13/cobra"
 )
 
 // listCmd represents the list command
 var listCmd = &cobra.Command{
 	Use:   "list",
-	Short: "A brief description of your command",
-	Long: `A longer description that spans multiple lines and likely contains examples
-and usage of using your command. For example:
+	Short: "list all notes",
+	Long: `List all notes:
 
-Cobra is a CLI library for Go that empowers applications.
-This application is a tool to generate the needed files
-to quickly create a Cobra application.`,
+Usage:
+simple-jot list`,
 	Run: func(cmd *cobra.Command, args []string) {
-		fmt.Println("list called")
+		noteList, err := storage.GetNotes()
+		if err != nil {
+			log.Fatal("cannot fetch notes. See error:", err.Error())
+		}
+		exNote := notes.Note{}
+		headers := exNote.GetHeaders()
+		dataFrame := make([][]string, len(noteList))
+		for i, n := range noteList {
+			dataFrame[i] = n.PrepRow()
+		}
+		table := tablewriter.NewWriter(os.Stdout)
+		table.Header(headers)
+		table.Bulk(dataFrame)
+		err = table.Render()
+		if err != nil {
+			log.Fatalln("Failed to render table for your notes query. Exiting Program")
+		}
 	},
 }
 
